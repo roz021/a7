321ccfb40ef050b23fd81c69e887694cfb979855
Roy Zhang <royzhang@roys-mbp-4.home>
2018-12-05 20:39:32 -0800

    finished sign up and home

--- js/signup3.js
+++ js/signup3.js
@@ -1,4 +1,42 @@
     function storeInterests(){
+        var aerobic = document.getElementById('aer');
+        var bbuild = document.getElementById('bb');
+        var card = document.getElementById('cc');
+        var dance = document.getElementById('d');
+        var mar = document.getElementById('ma');
+        var pow = document.getElementById('pl');
+        var sport = document.getElementById('sp');
+        var train = document.getElementById('tr');
+        var weight = document.getElementById('wl');
+
+        if(aerobic.style.opacity == 0.5){
+            allInt.push("Aerobics");
+        }
+        if(bbuild.style.opacity == 0.5){
+            allInt.push("BodyBuilding");
+        }
+        if(card.style.opacity == 0.5){
+            allInt.push("Cardio");
+        }
+        if(dance.style.opacity == 0.5){
+            allInt.push("Dance");
+        }
+        if(mar.style.opacity == 0.5){
+            allInt.push("Martial-Arts");
+        }
+        if(pow.style.opacity == 0.5){
+            allInt.push("PowerLifting");
+        }
+        if(sport.style.opacity == 0.5){
+            allInt.push("Sports");
+        }
+        if(train.style.opacity == 0.5){
+            allInt.push("Training");
+        }
+        if(weight.style.opacity == 0.5){
+            allInt.push("WeightLifting");
+        }
+
         localStorage.setItem('interests', JSON.stringify(allInt));
         location.href="../home.html";
     }
